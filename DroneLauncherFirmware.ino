b6c2a8ab5fb509f3d66df0041ce306334b81d93d
tjt22 <114026856+tjt22@users.noreply.github.com>
2023-09-08 16:00:15 -0400

    Create DroneLauncherFirmware.ino
    Creates BLE service on NRF Seed Studio and activates digital pin D0

---
+++ DroneLauncherFirmware.ino
@@ -0,0 +1,71 @@
+#include <ArduinoBLE.h>
+ 
+BLEService ledService("19B10000-E8F2-537E-4F6C-D104768A1214"); // Bluetooth® Low Energy LED Service
+ 
+// Bluetooth® Low Energy LED Switch Characteristic - custom 128-bit UUID, read and writable by central
+BLEByteCharacteristic switchCharacteristic("19B10001-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite);
+ 
+const int transistorPin = D0; // change based on where launcher is plugged in
+ 
+void setup() {
+  Serial.begin(9600);
+ 
+  // set LED pin to output mode
+  pinMode(transistorPin, OUTPUT);
+ 
+  // begin initialization
+  if (!BLE.begin()) {
+    Serial.println("starting Bluetooth® Low Energy module failed!");
+ 
+    while (1);
+  }
+ 
+  // set advertised local name and service UUID:
+  BLE.setLocalName("LAUNCHER");
+  BLE.setAdvertisedService(ledService);
+ 
+  // add the characteristic to the service
+  ledService.addCharacteristic(switchCharacteristic);
+ 
+  // add service
+  BLE.addService(ledService);
+ 
+  // set the initial value for the characeristic:
+  switchCharacteristic.writeValue(0);
+ 
+  // start advertising
+  BLE.advertise();
+ 
+  Serial.println("BLE LED Peripheral");
+}
+ 
+void loop() {
+  // listen for Bluetooth® Low Energy peripherals to connect:
+  BLEDevice central = BLE.central();
+ 
+  // if a central is connected to peripheral:
+  if (central) {
+    Serial.print("Connected to central: ");
+    // print the central's MAC address:
+    Serial.println(central.address());
+ 
+    // while the central is still connected to peripheral:
+  while (central.connected()) {
+        if (switchCharacteristic.written()) {
+          if (switchCharacteristic.value()) {   
+            digitalWrite(transistorPin, LOW); // changed from HIGH to LOW   
+  
+          } else {                              
+            digitalWrite(transistorPin, HIGH); // changed from LOW to HIGH
+            // optional:
+            // delay(4)
+            // digitalWrite(transistorPin, LOW);       
+          }
+        }
+      }
+ 
+    // when the central disconnects, print it out:
+    Serial.print(F("Disconnected from central: "));
+    Serial.println(central.address());
+  }
+}
